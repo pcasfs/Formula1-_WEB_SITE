ddc482363f45a3ed7f9e823cff900c3fc6972714
이동훈 <ldh020325@naver.com>
2025-08-06 16:02:50 +0900

    Feat: "기본 레이아웃 컴포넌트(DefaultLayout) 추가"

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,35 @@
 import Drivers from "./pages/Drivers/index";
+import DriverDetail from "./pages/DriverDetail";
 import Home from "./pages/Home";
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Header from "./components/Header/Header";
-import { ErrorBoundary } from "react-error-boundary";
+import Ranking from "./pages/Ranking";
+import RaceSchedules from "./pages/RaceSchedules";
+import RaceDetail from "./pages/RaceDetail";
+
+function DefaultLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+    </>
+  );
+}
 
 function App() {
   return (
     <>
-      <Header />
-      <ErrorBoundary fallback={<div>에러발생!</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/drivers" element={<Drivers />} />
-        </Routes>
-      </ErrorBoundary>
+      <Routes>
+        <Route path="/" element={<DefaultLayout />}>
+          <Route index element={<Home />} />
+          <Route path="drivers" element={<Drivers />} />
+          <Route path="drivers/:id" element={<DriverDetail />} />
+          <Route path="ranking" element={<Ranking />} />
+          <Route path="schedule" element={<RaceSchedules />} />
+          <Route path="race/:id" element={<RaceDetail />} />
+          <Route path="*" element={<div>페이지를 찾을 수 없습니다</div>} />
+        </Route>
+      </Routes>
     </>
   );
 }
