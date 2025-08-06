fe7309c87d94a38724f60db2427c9ced745e4375
이동훈 <ldh020325@naver.com>
2025-08-06 15:52:23 +0900

    Design: "클래스 네임 BRM방식으로 통일"

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,27 @@
-import { Link } from "react-router-dom";
-import f1__logo from "../../image/f1__logo.jpg";
+import { Link, useNavigate } from "react-router-dom";
+import f1__logo from "../../asset/image/f1__logo.jpg";
 import styles from "./Header.module.css";
+
 export default function Header() {
+  const navigate = useNavigate();
+
   return (
-    <div className={styles.container__header}>
-      <button>
-        <img className={styles.f1__logo} src={f1__logo} alt="f1__logo" />
+    <div className={styles.header}>
+      <button onClick={() => navigate("/")}>
+        <img className={styles["header__logo"]} src={f1__logo} alt="f1 logo" />
       </button>
 
-      <nav className={styles.wrapper__menu}>
-        <Link className={styles.menu__link} to="/drivers">
+      <nav className={styles["header__menu"]}>
+        <Link className={styles["header__menu-link"]} to="/drivers">
           드라이버
         </Link>
-        <Link className={styles.menu__link} to="/teams">
+        <Link className={styles["header__menu-link"]} to="/teams">
           팀
         </Link>
-        <Link className={styles.menu__link} to="/ranking">
+        <Link className={styles["header__menu-link"]} to="/ranking">
           랭킹
         </Link>
-        <Link className={styles.menu__link} to="/races">
+        <Link className={styles["header__menu-link"]} to="/schedule">
           경기일정
         </Link>
       </nav>
