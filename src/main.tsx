2be8c965967306762a2ab1d726009f3855080bec
이동훈 <ldh020325@naver.com>
2025-08-13 14:42:40 +0900

    Feat: "리액트 쿼리 전역옵션 추가 및 ScrollToTop 적용"

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,20 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop/ScrollToTop.tsx";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      gcTime: 1000 * 60 * 30,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+    <ScrollToTop />
     <QueryClientProvider client={queryClient}>
       <App />
     </QueryClientProvider>
