e18a8eb1ef22978f301defde1c5eadf1ae200bf9
이동훈 <ldh020325@naver.com>
2025-08-14 04:10:46 +0900

    Feat: "드라이버 콜백 이미지 추가"

--- src/constants/fallbackImages.ts
+++ src/constants/fallbackImages.ts
@@ -1,6 +1,8 @@
 import fallbackCircuit from "../asset/images/fallback__circuit.png";
+import fallbackDriver from "../asset/images/fallback__driver.svg";
 
 export const FALLBACK_IMAGES = {
   car: "https://media.formula1.com/image/upload/c_lfill,h_224/q_auto/d_common:f1:2025:fallback:car:2025fallbackcarright.webp/v1740000000/common",
   circuit: fallbackCircuit,
+  driver: fallbackDriver,
 };
